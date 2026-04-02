fc9304f1c7ecdbe74d189dc1e777ea9abfce4245
Suraj Nair <surajnair9698@gmail.com>
2026-04-03 00:09:51 +0530

    style: Navbar and Sidebar initial styling

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Navbar() {
-  return <div className="bg-blue-400 h-24">Navbar</div>;
+  return (
+    <div className="bg-blue-400 flex justify-between p-2.5">
+      <div className="">
+        <h1 className="text-3xl font-bold">Finance Dashboard</h1>
+      </div>
+      <div className="flex gap-10">
+        <div className="">
+          <select name="" id="">
+            <option value="">Admin</option>
+            <option value="">View</option>
+          </select>
+        </div>
+        <div>🌙</div>
+        <div>⚙️</div>
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
--- src/layout/Sidebar.jsx
+++ src/layout/Sidebar.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "/favicon.ico";
 
 function Sidebar() {
   return (
-    <div className="h-screen bg-red-400 w-1/8">
-      <div>
-        <Link to="/">Dashboard</Link>
+    <div className="bg-linear-to-r bg-black to-blue-900 border-r border-gray-500">
+      <div className="flex items-center border-b border-gray-500 px-7 py-2 gap-2">
+        <img src={logo} className="h-10 rounded-full" alt="" />
+        <h1 className="text-3xl font-bold">FinTrack</h1>
       </div>
-      <div>
-        <Link to="/transactions">Transaction</Link>
-      </div>
-      <div>
-        <Link to="/insights">Insight</Link>
+      <div className=" flex flex-col gap-2 p-5">
+        <div className="w-full p-2 bg-gray-600 rounded-full">
+          <Link to="/">🛖 Dashboard</Link>
+        </div>
+        <div className="w-full p-2">
+          <Link to="/transactions"> 💸 Transaction</Link>
+        </div>
+        <div className="w-full p-2">
+          <Link to="/insights">📊 Insight</Link>
+        </div>
       </div>
     </div>
   );
